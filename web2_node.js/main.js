1692aecbad49703c29ca14d866deb61fa3af2b20
Gwanghyeon Seo <dya2122@gmail.com>
2019-12-26 23:36:57 -0600

    3일차 pathname 및 NotFound 구현

--- web2_node.js/main.js
+++ web2_node.js/main.js
@@ -1,52 +1,75 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
 //CRUD
 //Create
 //Read
 //Update
 //Delete
 // File 을 생성 수정 삭제 등
+console.log('hello');
+var app = http.createServer(function(request, response) {
+  var _url = request.url;
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-    var queryData=url.parse(_url, true).query;
-
-    var title =queryData.id;
-
-    if(_url == '/'){// _url의 첫 값이 /이면  index.thml 이다.
-      title= 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf8', function (err, description){
-      var template= `
+  console.log(url.parse(_url, true));
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      var title = 'Welcome';
+      var description = 'Hello, Node.js';
+      var template = `
       <!doctype html>
       <html>
       <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
       </head>
       <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=html">HTML</a></li>
-          <li><a href="/?id=css">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}  </p>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+      <li><a href="/?id=html">HTML</a></li>
+      <li><a href="/?id=css">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}  </p>
       </body>
       </html>
       `;
-          response.end(template);
-    });
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        var title = queryData.id;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+        <li><a href="/?id=html">HTML</a></li>
+        <li><a href="/?id=css">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}  </p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
 
-
-
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end('Not Found');
+  }
 });
 app.listen(3000);
