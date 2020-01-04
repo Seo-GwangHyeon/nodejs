6451bc16492326b8d66ad9490835830739012753
Gwanghyeon Seo <dya2122@gmail.com>
2020-01-03 23:28:32 -0600

    1D1C Day 2 파일 목록 읽어온 후 스크립트에 동적으로 반영 시키기

--- web2_node/main.js
+++ web2_node/main.js
@@ -7,19 +7,33 @@ var url = require('url');
 //Update
 //Delete
 // File 을 생성 수정 삭제 등
-console.log('hello');
 var app = http.createServer(function(request, response) {
   var _url = request.url;
 
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
+  var template = '';
 
-  console.log(url.parse(_url, true));
+  //console.log(url.parse(_url, true));
   if (pathname === '/') {
-    if (queryData.id === undefined) {
-      var title = 'Welcome';
-      var description = 'Hello, Node.js';
-      var template = `
+    var list = '';
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+
+    fs.readdir('./data/', function(err, filelist) {
+      list = '<ul>';
+      var i = 0;
+      while (i < filelist.length) {
+        list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i++;
+      }
+      list += '</ul>';
+
+
+      if (queryData.id === undefined) {
+        title = 'Welcome';
+        description = 'Hello, Node.js';
+        template = `
       <!doctype html>
       <html>
       <head>
@@ -28,22 +42,18 @@ var app = http.createServer(function(request, response) {
       </head>
       <body>
       <h1><a href="/">WEB</a></h1>
-      <ul>
-      <li><a href="/?id=html">HTML</a></li>
-      <li><a href="/?id=css">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
+      ${list}
       <h2>${title}</h2>
       <p>${description}  </p>
       </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-        var title = queryData.id;
-        var template = `
+      </html>`;
+        response.writeHead(200);
+        response.end(template);
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, filedescription) {
+          title = queryData.id;
+          description = filedescription;
+          template = `
         <!doctype html>
         <html>
         <head>
@@ -52,22 +62,18 @@ var app = http.createServer(function(request, response) {
         </head>
         <body>
         <h1><a href="/">WEB</a></h1>
-        <ul>
-        <li><a href="/?id=html">HTML</a></li>
-        <li><a href="/?id=css">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
+        ${list}
         <h2>${title}</h2>
         <p>${description}  </p>
         </body>
-        </html>
-        `;
+        </html>`;
         response.writeHead(200);
         response.end(template);
-
-      });
-    }
-  } else {
+        });
+      } //end else
+    })
+  } //end if path name
+  else {
     response.writeHead(404);
     response.end('Not Found');
   }
---
+++ web2_node/readdir.js
@@ -0,0 +1,8 @@
+var testFolder='./data'; // == 'data'
+var fs=require('fs');
+
+fs.readdir(testFolder, function(error, filelist)
+{
+  console.log(filelist);
+}
+)
